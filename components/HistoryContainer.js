f219459f46ade3aa7953b8ebcb796d04634bce44
Jason <47071224+JasonHalvorson@users.noreply.github.com>
2022-01-31 18:16:15 -0800

    Make history overlay colour darker

--- components/HistoryContainer.js
+++ components/HistoryContainer.js
@@ -9,7 +9,7 @@ export default function HistoryContainer({ history, open, setOpen }) {
             <Dialog as="div" className="fixed inset-0 overflow-hidden" onClose={setOpen}>
                 <div className="absolute inset-0 overflow-hidden">
                     <Transition.Child as={Fragment} enter="ease-in-out duration-500" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in-out duration-500" leaveFrom="opacity-100" leaveTo="opacity-0">
-                        <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        <Dialog.Overlay className="absolute inset-0 bg-gray-900 bg-opacity-75 transition-opacity" />
                     </Transition.Child>
                     <div className="fixed inset-y-0 right-0 pl-10 max-w-full flex">
                         <Transition.Child as={Fragment} enter="transform transition ease-in-out duration-500 sm:duration-700" enterFrom="translate-x-full" enterTo="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leaveFrom="translate-x-0" leaveTo="translate-x-full">
